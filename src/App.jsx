3a2470d10cd2c1d0f58d25d01b0730706689d1c3
Madhav Peri <prn.madhav@gmail.com>
2026-06-22 00:00:25 -0500

    Add outlet child components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
-import NavBar from './NavBar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Body from './Body'
+import Login from './Login'
+import Profile from './Profile'
 
 function App() {
 
   return (
     <>
-      <NavBar />
-      <h1 className="text-3xl font-bold">Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />} >
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => (
+    <footer className="footer sm:footer-horizontal footer-center bg-base-200 text-base-content p-4 fixed bottom-0">
+        <aside>
+            <p>Copyright © {new Date().getFullYear()} - All right reserved by DevTinder Industries Ltd</p>
+        </aside>
+    </footer>
+)
+
+export default Footer
