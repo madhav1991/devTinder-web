6cf3b0b6fb472885625d48aa734a83ccdfba9441
Madhav Peri <prn.madhav@gmail.com>
2026-06-23 23:10:19 -0500

    Setup redux store

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,29 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
 
 const Login = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const handleLogin = async () => {
         try {
             const res = await axios.post(
-                "http://localhost:3000/login",
+                BASE_URL + "/login",
                 {
                     email,
                     password,
                 },
                 { withCredentials: true }
             );
+            dispatch(addUser(res.data))
+            navigate("/")
         } catch (err) {
             console.log(err)
         }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const NavBar = () => {
+    const user = useSelector(store => store.user)
     return (
         <div className="navbar bg-base-300 shadow-sm">
             <div className="flex-1">
@@ -8,12 +10,13 @@ const NavBar = () => {
             </div>
             <div className="flex gap-2">
                 <div className="dropdown dropdown-end mx-5">
+                    <div>{user?.firstName}</div>
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
+                        {user && <div className="w-10 rounded-full">
                             <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                        </div>
+                                alt="Display photo"
+                                src={user.photoUrl ? user.photoUrl : "https://api.dicebear.com/8.x/avataaars/svg?seed=Felix"} />
+                        </div>}
                     </div>
                     <ul
                         tabIndex="-1"
