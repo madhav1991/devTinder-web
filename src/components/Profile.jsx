90fa233258bb26874a25ad0457fd43ae3a84e168
Madhav Peri <prn.madhav@gmail.com>
2026-06-28 17:39:10 -0500

    Add edit profile functionality

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import EditProfile from "./EditProfile.jsx";
+import { useSelector } from 'react-redux'
 
-const Profile = () => (
-    <div>
-        Profile
-    </div>
-)
-
+const Profile = () => {
+    const user = useSelector((store) => store.user);
+    return (
+        user && (
+            <div>
+                <EditProfile user={user} />
+            </div>
+        )
+    );
+};
 export default Profile
